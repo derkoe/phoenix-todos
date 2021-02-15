a19fad4bb934ebd130e8680f23fdc31b84a178ca
Christian Köberl <christian.koeberl@porscheinformatik.com>
2021-02-15 23:40:52 +0100

    feat: Add double-click edit handling

--- lib/todos_web/live/todo_live/item_component.ex
+++ lib/todos_web/live/todo_live/item_component.ex
@@ -5,9 +5,7 @@ defmodule TodosWeb.TodoLive.ItemComponent do
   @spec update(maybe_improper_list | map, Phoenix.LiveView.Socket.t()) ::
           {:ok, Phoenix.LiveView.Socket.t()}
   def update(assigns, socket) do
-    {:ok,
-     socket
-     |> assign(assigns)
-     |> assign(:editing, false)}
+    {:ok, assign(socket, assigns)}
   end
+
 end
