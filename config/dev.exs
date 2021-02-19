6d5e048af66eed44f2531f6cc5ca495bbb003857
Christian Köberl <christian.koeberl@porscheinformatik.com>
2021-02-19 20:03:52 +0100

    Add vscode dev container configuration

--- config/dev.exs
+++ config/dev.exs
@@ -1,13 +1,24 @@
 use Mix.Config
 
 # Configure your database
-config :todos, Todos.Repo,
-  username: "todos",
-  password: "password",
-  database: "todos",
-  hostname: "localhost",
-  show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+if System.get_env("REMOTE_CONTAINERS") == "true" do
+  config :todos, Todos.Repo,
+    username: "todos",
+    password: "password",
+    database: "todos",
+    hostname: "database",
+    show_sensitive_data_on_connection_error: true,
+    pool_size: 10
+else
+  # Configure your database
+  config :todos, Todos.Repo,
+    username: "todos",
+    password: "password",
+    database: "todos",
+    hostname: "localhost",
+    show_sensitive_data_on_connection_error: true,
+    pool_size: 10
+end
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
